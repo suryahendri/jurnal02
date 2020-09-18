4b246bf67154ad6277939b168ad86fc98f49e49b
suryahendri <71318542+suryahendri@users.noreply.github.com>
2020-09-18 09:28:23 +0700

    Add files via upload

---
+++ Jurnal02surya/Jurnal-02-master/Jurnal2surya/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main (String[] args){
+		System.out.println("Nama: Surya Hendriansyah");
+		System.out.println("NIM: 6706200113");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+}	
